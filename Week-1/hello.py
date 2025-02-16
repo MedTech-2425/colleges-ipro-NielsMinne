38c197b31bf18fa7e3caadaad9ad05532e8c88e9
Niels Minne <nielsminne@gmail.com>
2025-02-16 16:50:32 +0100

    Added week 2

---
+++ Week-1/hello.py
@@ -0,0 +1,15 @@
+# printen van een string
+print("Hello, MedTech!")
+
+# Wat bij een fout?
+print("Hello, MedTech!"
+# SyntaxError: '(' was never closed
+
+#input laat je iets invoeren in de terminal 
+print("Wat is je naam?")
+input()
+
+#input kan ook een string bevatten
+input("Wat is je naam? ")
+
+
